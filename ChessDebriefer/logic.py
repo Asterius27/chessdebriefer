b677a0a94c30ffaf1c2a29de91af4c7f8cc449e7
Asterius27 <878352@stud.unive.it>
2022-03-09 14:48:40 +0100

    added cache update script

--- ChessDebriefer/logic.py
+++ ChessDebriefer/logic.py
@@ -9,8 +9,12 @@ from ChessDebriefer.models import Games, Players, FieldsCache
 
 
 # only works with 1 file upload at a time, and it takes a lot of time to parse everything
-# TODO update cache
 def handle_pgn_uploads(f):
+    cached_players = Players.objects
+    cached_fields = FieldsCache.objects.first()
+    fields = ["event", "opening", "termination"]
+    if not cached_fields:
+        cached_fields = FieldsCache(event=[], opening=[], termination=[]).save()
     with open('temp.pgn', 'wb+') as temp:
         for chunk in f.chunks():
             temp.write(chunk)
@@ -22,14 +26,31 @@ def handle_pgn_uploads(f):
             arr = game.headers["UTCDate"].split(".")
             date = datetime.datetime(int(arr[0]), int(arr[1]), int(arr[2]))
             if game.headers["Black"] != "?" and game.headers["White"] != "?":
-                Games(event=game.headers["Event"], site=game.headers["Site"], white=game.headers["White"],
-                      black=game.headers["Black"], result=game.headers["Result"], date=date,
-                      white_elo=game.headers["WhiteElo"], black_elo=game.headers["BlackElo"],
-                      white_rating_diff=game.headers["WhiteRatingDiff"],
-                      black_rating_diff=game.headers["BlackRatingDiff"], eco=game.headers["ECO"],
-                      opening=game.headers["Opening"], time_control=game.headers["TimeControl"],
-                      termination=game.headers["Termination"], moves=str(game.mainline_moves()), best_moves=[],
-                      moves_evaluation=[]).save()
+                saved_game = Games(event=game.headers["Event"], site=game.headers["Site"], white=game.headers["White"],
+                                   black=game.headers["Black"], result=game.headers["Result"], date=date,
+                                   white_elo=game.headers["WhiteElo"], black_elo=game.headers["BlackElo"],
+                                   white_rating_diff=game.headers["WhiteRatingDiff"],
+                                   black_rating_diff=game.headers["BlackRatingDiff"], eco=game.headers["ECO"],
+                                   opening=game.headers["Opening"], time_control=game.headers["TimeControl"],
+                                   termination=game.headers["Termination"], moves=str(game.mainline_moves()),
+                                   best_moves=[], moves_evaluation=[]).save()
+                for field in fields:
+                    if "https" in getattr(saved_game, field):
+                        new = saved_game.event.split(" ")
+                        del new[-1]
+                        if ' '.join(new) not in getattr(cached_fields, field):
+                            temp = getattr(cached_fields, field)
+                            temp.append(' '.join(new))
+                            setattr(cached_fields, field, temp)
+                            cached_fields.save()
+                    elif getattr(saved_game, field) not in getattr(cached_fields, field):
+                        temp = getattr(cached_fields, field)
+                        temp.append(getattr(saved_game, field))
+                        setattr(cached_fields, field, temp)
+                        cached_fields.save()
+                for player in cached_players:
+                    if saved_game.white == player.name or saved_game.black == player.name:
+                        player.delete()
     os.remove("temp.pgn")
 
 
@@ -124,25 +145,8 @@ def calculate_percentages(name, params):
 
 def filter_games(games, name, field):
     fields_cache = FieldsCache.objects.first()
-    if not fields_cache:
-        fields_cache = FieldsCache(event=[], opening=[], termination=[]).save()
     result = {}
-    if not getattr(fields_cache, field):
-        temps = Games.objects()
-        fields = []
-        for temp in temps:
-            if "https" in getattr(temp, field):
-                new = temp.event.split(" ")
-                del new[-1]
-                if ' '.join(new) not in fields:
-                    fields.append(' '.join(new))
-            elif getattr(temp, field) not in fields:
-                fields.append(getattr(temp, field))
-        setattr(fields_cache, field, fields)
-        fields_cache.save()
-    else:
-        fields = getattr(fields_cache, field)
-    for fld in fields:
+    for fld in getattr(fields_cache, field):
         filtered_games = filter(lambda game: getattr(game, field) == fld, games)
         dictionary = create_dictionary(filtered_games, name)
         if dictionary:
