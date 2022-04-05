67e35d0e51f1b236bee43fc60eae626861fcdb2f
Asterius27 <878352@stud.unive.it>
2022-04-05 18:20:54 +0200

    added complete openings charts

--- frontend/src/components/OpeningsChart.js
+++ frontend/src/components/OpeningsChart.js
@@ -15,19 +15,42 @@ function OpeningsChart({ eco, url }) {
     const [data, setData] = useState({})
 
     if (Object.keys(data).length !== 0) {
-        
+
         let generalChartData = {
             labels: ['White Wins', 'BLack Wins', 'Draws'],
             datasets: [{
-            label: 'wdl',
-            data: [data["data"][eco]["white_wins"], data["data"][eco]["black_wins"], data["data"][eco]["draws"]],
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
+                label: 'wdl',
+                data: [data["data"][eco]["white_wins"], data["data"][eco]["black_wins"], data["data"][eco]["draws"]],
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
             }]
+        }
+
+        let variationsChartsData = []
+        let variationNames = []
+        let variationEngineEvals = []
+        for (const variation in data["data"]["variations"]) {
+            if (data["data"]["variations"][variation]["white_wins"] + data["data"]["variations"][variation]["black_wins"] + data["data"]["variations"][variation]["draws"] !== 0) {
+                variationEngineEvals.push(data["data"]["variations"][variation]["engine_evaluation"])
+                variationNames.push(variation)
+                variationsChartsData.push({
+                    labels: ['White Wins', 'BLack Wins', 'Draws'],
+                    datasets: [{
+                        label: 'wdl',
+                        data: [data["data"]["variations"][variation]["white_wins"], data["data"]["variations"][variation]["black_wins"], data["data"]["variations"][variation]["draws"]],
+                        backgroundColor: [
+                            'rgb(255, 99, 132)',
+                            'rgb(54, 162, 235)',
+                            'rgb(255, 205, 86)'
+                        ],
+                        hoverOffset: 4
+                    }]
+                })
+            }
         }
 
         const doughnutStyle = {
@@ -38,8 +61,21 @@ function OpeningsChart({ eco, url }) {
         }
 
         return (
-            <div style={doughnutStyle}>
-                <DoughnutChart chartData={generalChartData} text={eco + " Side Advantage"} />
+            <div>
+                <div style={doughnutStyle}>
+                    <DoughnutChart chartData={generalChartData} text={eco + " Side Advantage"} />
+                </div>
+                <div>
+                    <h1 style={{fontSize: "700%"}}>Variations</h1>
+                </div>
+                {variationsChartsData.map(function(variationsChartData, i){
+                    return (
+                        <div key={variationNames[i]} style={doughnutStyle}>
+                            <DoughnutChart chartData={variationsChartData} text={variationNames[i]} />
+                            <p>Engine Evaluation: {variationEngineEvals[i]}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
