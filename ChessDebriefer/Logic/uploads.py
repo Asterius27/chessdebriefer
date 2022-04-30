df352a11457b15bbb713b07fcc53bc266b29bd00
Asterius27 <878352@stud.unive.it>
2022-04-30 17:50:06 +0200

    working on making multithread faster

--- ChessDebriefer/Logic/uploads.py
+++ ChessDebriefer/Logic/uploads.py
@@ -19,75 +19,95 @@ def handle_pgn_uploads(f):
         with open(file_name, 'wb+') as temp:
             for chunk in f.chunks():
                 temp.write(chunk)
-        thr = threading.Thread(target=parse_pgn, args=(file_name,))
+        thr = threading.Thread(target=parse_pgn, args=(file_name, i))
         thr.start()
 
 
+# TODO try with n = 100 and n = 1
 # n = 10 -> 34 min per 121114 partite
 # n = 1 -> 1 ora e 49 min per 121114 partite
 # n = 10 -> 28 min per 179207 partite with index
 # n = 10 -> 19 min per 121114 partite with index
 # n = 1 -> 42 min per 121114 partite with index
 # n = 20 -> 20 min per 121114 partite with index
-def parse_pgn(file_name):
+# n = 10 -> 20 min per 121114 partite with index and separated files
+# n = 20 -> 20 min per 121114 partite with index and separated files
+def parse_pgn(file_name, ind):
     # cached_fields = FieldsCache.objects.first()
     # fields = ["event", "termination"]
     # if not cached_fields:
     #    cached_fields = FieldsCache(event=[], opening_id=[], eco=[], termination=[]).save()
     with open(file_name) as pgn:
-        n = 10
-        lock = threading.Lock()
+        n = 100
+        lines = pgn.readlines()
+        l = len(lines)
+        j = 0
+        h = 0
+        file = open("temp" + str(ind) + str(j) + ".pgn", 'w+')
+        for i in range(l - 1):
+            if lines[i] == "\n" and h > ((l / n) * 1.) and lines[i + 1].startswith("["):
+                file.write(lines[i])
+                file.close()
+                h = 0
+                j += 1
+                file = open("temp" + str(ind) + str(j) + ".pgn", 'w+')
+            else:
+                file.write(lines[i])
+            h += 1
+        file.write(lines[l - 1])
+        file.close()
         futures = []
         with concurrent.futures.ThreadPoolExecutor(max_workers=n) as executor:
             print(datetime.datetime.now())
             for x in range(n):
-                future = executor.submit(run, pgn, lock)
+                future = executor.submit(run, x, ind)
                 futures.append(future)
             concurrent.futures.wait(futures)
     print(datetime.datetime.now())
     os.remove(file_name)
 
 
-def run(pgn, lock):
-    while True:
-        with lock:
+def run(i, ind):
+    with open("temp" + str(ind) + str(i) + ".pgn") as pgn:
+        while True:
             game = chess.pgn.read_game(pgn)
-        if game is None:
-            break
-        if game.headers["Black"] != "?" and game.headers["White"] != "?" and game.headers["UTCDate"] != "?" and \
-                game.headers["UTCTime"] != "?":
-            arr_date = game.headers["UTCDate"].split(".")
-            arr_time = game.headers["UTCTime"].split(":")
-            date = datetime.datetime(int(arr_date[0]), int(arr_date[1]), int(arr_date[2]), int(arr_time[0]),
-                                     int(arr_time[1]), int(arr_time[2]))
-            if "https" in game.headers["Event"]:
-                temp = game.headers["Event"].split(" ")
-                tournament_site = temp[-1]
-                del temp[-1]
-                event = ' '.join(temp)
-            else:
-                tournament_site = ""
-                event = game.headers["Event"]
-            fen = ""
-            if reaches_five_piece_endgame(game):
-                fen = endgame_start_fen(game.end())
-            saved_game = Games(event=event, tournament_site=tournament_site, site=game.headers["Site"],
-                               white=game.headers["White"], black=game.headers["Black"],
-                               result=game.headers["Result"], date=date, white_elo=game.headers["WhiteElo"],
-                               black_elo=game.headers["BlackElo"],
-                               white_rating_diff=game.headers["WhiteRatingDiff"],
-                               black_rating_diff=game.headers["BlackRatingDiff"], eco="",
-                               opening_id="000000000000000000000000", time_control=game.headers["TimeControl"],
-                               termination=game.headers["Termination"], moves=str(game.mainline_moves()),
-                               best_moves=[], moves_evaluation=[], five_piece_endgame_fen=fen)
-            try:
-                saved_game.save()
-                find_opening(saved_game)
-            except:
-                saved_game.delete()
-            # update_cache(saved_game, fields, cached_fields)
-            # update_player_cache(saved_game.white, saved_game.white_elo, saved_game)
-            # update_player_cache(saved_game.black, saved_game.black_elo, saved_game)
+            if game is None:
+                break
+            if game.headers["Black"] != "?" and game.headers["White"] != "?" and game.headers["UTCDate"] != "?" and \
+                    game.headers["UTCTime"] != "?":
+                arr_date = game.headers["UTCDate"].split(".")
+                arr_time = game.headers["UTCTime"].split(":")
+                date = datetime.datetime(int(arr_date[0]), int(arr_date[1]), int(arr_date[2]), int(arr_time[0]),
+                                         int(arr_time[1]), int(arr_time[2]))
+                if "https" in game.headers["Event"]:
+                    temp = game.headers["Event"].split(" ")
+                    tournament_site = temp[-1]
+                    del temp[-1]
+                    event = ' '.join(temp)
+                else:
+                    tournament_site = ""
+                    event = game.headers["Event"]
+                fen = ""
+                if reaches_five_piece_endgame(game):
+                    fen = endgame_start_fen(game.end())
+                saved_game = Games(event=event, tournament_site=tournament_site, site=game.headers["Site"],
+                                   white=game.headers["White"], black=game.headers["Black"],
+                                   result=game.headers["Result"], date=date, white_elo=game.headers["WhiteElo"],
+                                   black_elo=game.headers["BlackElo"],
+                                   white_rating_diff=game.headers["WhiteRatingDiff"],
+                                   black_rating_diff=game.headers["BlackRatingDiff"], eco="",
+                                   opening_id="000000000000000000000000", time_control=game.headers["TimeControl"],
+                                   termination=game.headers["Termination"], moves=str(game.mainline_moves()),
+                                   best_moves=[], moves_evaluation=[], five_piece_endgame_fen=fen)
+                try:
+                    saved_game.save()
+                    find_opening(saved_game)
+                except:
+                    saved_game.delete()
+                # update_cache(saved_game, fields, cached_fields)
+                # update_player_cache(saved_game.white, saved_game.white_elo, saved_game)
+                # update_player_cache(saved_game.black, saved_game.black_elo, saved_game)
+    os.remove("temp" + str(ind) + str(i) + ".pgn")
 
 
 def reaches_five_piece_endgame(parsed_game):
