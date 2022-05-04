acc98e19886472f417f61c00b5dca725520e5277
Asterius27 <878352@stud.unive.it>
2022-05-04 13:49:16 +0200

    todo

--- ChessDebriefer/settings.py
+++ ChessDebriefer/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# TODO add frontend ip here
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000'
