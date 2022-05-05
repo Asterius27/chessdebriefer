5a63bf52261ae5118675cb510b25fa6910b651e4
Asterius27 <878352@stud.unive.it>
2022-05-05 12:59:10 +0200

    cors fix

--- ChessDebriefer/settings.py
+++ ChessDebriefer/settings.py
@@ -50,11 +50,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# TODO add frontend ip here
+CORS_ALLOW_ALL_ORIGINS = True
+"""
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000'
 ]
+"""
 
 ROOT_URLCONF = 'ChessDebriefer.urls'
 
